9c8f01892d187399a863890908c163817bfd06e3
HuongPTT03 <huong.phi@netko-solution.com>
2024-09-26 09:06:33 +0700

    commit first1

---
+++ new_folder/git.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Git = () => {
+  return (
+    <div>
+      <span>git commit --amend</span>
+      <span>git push --force/ git push -f</span>
+    </div>
+  )
+}
